171b7c284bac52470b75e1dc2c8a3d24bf051b6e
Ramil Mamedov <ramil.mamedov7991@gmail.com>
2017-07-06 17:28:04 +0400

    Add files via upload

---
+++ Tək cüt/Tək cüt.js
@@ -0,0 +1,20 @@
+var a, b, c;
+if (a % 2 == 0); {
+    document.write("Yes");
+}
+if
+else(a % 2 == 0) {
+        document.write("No");
+        else(b % 2 == 0) {
+            document.write("Yes");
+        }
+        if
+        else(b % 2 == 0) {
+            document.write("No");
+            else(c % 2 == 0) {
+                document.write("Yes");
+            }
+            if
+            else(c % 2 == 0)
+            ocument.write("No");
+        }
